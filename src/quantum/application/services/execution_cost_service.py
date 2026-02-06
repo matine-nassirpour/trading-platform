6bf5538a2e41427e5ff820e3def8ae12445d5a24
Matine NASSIRPOUR <matine@nassirpour.org>
2026-02-06 13:26:19 +0100

    fix(application): remove useless services

--- src/quantum/application/services/execution_cost_service.py
+++
@@ -1,8 +0,0 @@
-from quantum.domain.trading.execution.settlement.execution_cost import ExecutionCost
-
-
-class ExecutionCostService:
-
-    @staticmethod
-    def record_cost(cost: ExecutionCost) -> ExecutionCost:
-        return cost
